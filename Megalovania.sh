cd3557a58bce98d9cce3c7237a20cc2fbf1523fd
PigcraftDev <91039432+PigcraftDev@users.noreply.github.com>
2021-10-04 18:13:34 +1300

    Update Megalovania.sh

--- Megalovania.sh
+++ Megalovania.sh
@@ -1,5 +1,11 @@
 #I honestly DO NOT care what you do with this
-#For all I care You could literaly sell it for a million dollars, take credit and win an award for it (It only took  me an hour
+#For all I care You could literaly sell it for a million dollars, take credit and win an award for it (It only took  me an hour)
+
+ver='1.0.0'
+echo MEGALOVANIA BEEP $ver
+
+
+
 
 for i in {1..2}; do
     beep -f 293.66 -l 125 -n -f 293.66 -l 125 -D 125 -n -f 587.33 -l 250 -D 125 -n -f 440.00 -l 250 -D 125 -n -f 415.30 -l 125 -D 125 -n  -f 392.00 -l 125 -D 125 -n -f 349.23 -l 250 -n -f 293.66 -l 125 -n -f 349.23 -l 125 -n -f 392.00 -l 125 -n -f 261.63 -l 125 -n -f 261.63 -l 125 -D 125 -n -f 587.33 -l 250 -D 125 -n -f 440.00 -l 250 -D 125 -n -f 415.30 -l 125 -D 125 -n  -f 392.00 -l 125 -D 125 -n -f 349.23 -l 250 -n -f 293.66 -l 125 -n -f 349.23 -l 125 -n -f 392.00 -l 125 -n -f 246.94 -l 125 -n -f 246.94 -l 125 -D 125 -n -f 587.33 -l 250 -D 125 -n -f 440.00 -l 250 -D 125 -n -f 415.30 -l 125 -D 125 -n  -f 392.00 -l 125 -D 125 -n -f 349.23 -l 250 -n -f 293.66 -l 125 -n -f 349.23 -l 125 -n -f 392.00 -l 125 -n -f 233.08 -l 125 -n -f 233.08 -l 125 -D 125 -n -f 587.33 -l 250 -D 125 -n -f 440.00 -l 250 -D 125 -n -f 415.30 -l 125 -D 125 -n  -f 392.00 -l 125 -D 125 -n -f 349.23 -l 250 -n -f 293.66 -l 125 -n -f 349.23 -l 125 -n -f 392.00 -l 125;
